b663bbd5ffc1774f5cfebb094330a5487b45e6e2
jhonataT <jhonata.tenorio2526@gmail.com>
2023-05-31 15:32:22 -0300

    Connect database and create users files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule, UsersModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
