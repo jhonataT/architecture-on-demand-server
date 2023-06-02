f031124097278440e11aced83e448d5cdfad546b
jhonataT <jhonata.tenorio2526@gmail.com>
2023-06-02 13:06:46 -0300

    Hotfix: fixing database host

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DATABASE_HOSTNAME || 'pgsql',
+  host: process.env.DATABASE_HOSTNAME || 'architects-system',
   port: Number(process.env.DATABASE_PORT) || 5432,
   username: process.env.DATABASE_USERNAME || 'localuser',
   password: String(process.env.DATABASE_PASSWORD) || 'userpass',
