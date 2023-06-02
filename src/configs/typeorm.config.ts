9d4292bb74c9a702469f343b7008404455b6d385
jhonataT <jhonata.tenorio2526@gmail.com>
2023-06-02 12:48:09 -0300

    Adding settings to deploy

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -3,11 +3,11 @@ require('dotenv').config();
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DATABASE_HOSTNAME,
+  host: process.env.DATABASE_HOSTNAME || 'pgsql',
   port: Number(process.env.DATABASE_PORT) || 5432,
-  username: process.env.DATABASE_USERNAME,
-  password: String(process.env.DATABASE_PASSWORD),
-  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USERNAME || 'localuser',
+  password: String(process.env.DATABASE_PASSWORD) || 'userpass',
+  database: process.env. DATABASE_NAME || 'database',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: process.env.DATABASE_HOSTNAME === 'localhost',
+  synchronize: true,
 };
